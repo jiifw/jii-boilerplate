d369fb5365ca00d639803c4786eec78a13b98cf4
Junaid Atari <mj.atari@gmail.com>
2023-12-30 19:29:34 +0500

    implement class based functionality

--- src/sequelize/models/User.ts
+++ src/sequelize/models/User.ts
@@ -1,42 +1,57 @@
 import { DataTypes } from 'sequelize';
 
+// classes
+import BaseModel from '@framework/db/sequelize/classes/BaseModel';
+
 // utils
 import { getInstance } from '@framework/db/sequelize/instance';
-import use from '@framework/db/sequelize/use-trait';
 
 // types
+import { sequelize } from '@framework/db/sequelize/types';
 import { UserModel, UserModelStatic } from './User.types';
 
 /**
  * User model
- * @class User
+ * This is the model class for table "jii.public.users"
  */
-const User = <UserModelStatic<UserModel>>getInstance().define('User', {
+const User = <UserModelStatic<UserModel>>class User extends BaseModel implements UserModel {
+  /** List of traits to use */
+  public static readonly traits: Array<sequelize.TraitPath> = [
+    `#framework://attributes`,
+    `#framework://status`,
+    `#framework://type`,
+    //`#framework://json`,
+    //`#framework://pagination-number`,
+  ];
+
+  /** Status as active */
+  static STATUS_ACTIVE: number = 10;
+
+  /** Status as inactive */
+  static STATUS_INACTIVE: number = 9;
+
+  /** Status as archived */
+  static STATUS_ARCHIVED: number = 5;
+
+  /** Status as deleted */
+  static STATUS_DELETED: number = 0;
+};
+
+User.init({
   id: {
     type: DataTypes.INTEGER,
     autoIncrement: true,
     primaryKey: true,
   },
 }, {
+  sequelize: getInstance(),
   schema: 'public',
   tableName: 'users',
   timestamps: false,
   createdAt: false,
 });
 
-//<editor-fold desc="Status constants">
-User.STATUS_ACTIVE = 10;
-User.STATUS_ARCHIVED = 5;
-User.STATUS_DELETED = 0;
-//</editor-fold>
-
 // traits
-use(User, [
-  `#framework://attributes`,
-  `#framework://status`,
-  `#framework://type`,
-  //`#framework://json`,
-  //`#framework://pagination-number`,
-]);
+User.initTraits();
 
 export default <typeof User>User;
--- src/sequelize/models/User.types.ts
+++ src/sequelize/models/User.types.ts
@@ -1,6 +1,8 @@
 // types
-import { ModelStatic } from 'sequelize';
-import { Model } from '@framework/db/sequelize/base/Model';
+import BaseModel from '@framework/db/sequelize/classes/BaseModel';
+import ModelStatic from '@framework/db/sequelize/interfaces/ModelStatic';
+
+// traits types
 import { AttributesTrait, AttributesTraitStatic } from '@framework/db/sequelize/traits/attributes';
 import { JSONTrait, JSONTraitStatic } from '@framework/db/sequelize/traits/json';
 import { AuthTrait, AuthTraitStatic } from '@framework/db/sequelize/traits/auth';
@@ -9,7 +11,7 @@ import { PaginationNumberTrait, PaginationNumberTraitStatic } from '@framework/d
 import { StatusTrait, StatusTraitStatic } from '@framework/db/sequelize/traits/status';
 import { TypeTrait, TypeTraitStatic } from '@framework/db/sequelize/traits/type';
 
-export interface UserModel extends Model,
+export interface UserModel extends BaseModel,
   PaginationCursorTrait,
   PaginationNumberTrait,
   AttributesTrait,
@@ -17,9 +19,10 @@ export interface UserModel extends Model,
   AuthTrait,
   TypeTrait,
   StatusTrait {
+  id?: number;
 }
 
-export interface UserModelStatic<T extends Model> extends ModelStatic<T>,
+export interface UserModelStatic<T extends UserModel> extends ModelStatic<T>,
   PaginationCursorTraitStatic<T>,
   PaginationNumberTraitStatic<T>,
   AttributesTraitStatic<T>,
@@ -27,9 +30,6 @@ export interface UserModelStatic<T extends Model> extends ModelStatic<T>,
   AuthTraitStatic<T>,
   TypeTraitStatic<T>,
   StatusTraitStatic<T> {
-
-  id?: string;
-
   //<editor-fold desc="Status constants">
   /**
    * @default 10
